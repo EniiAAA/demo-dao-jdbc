f5a1139f54db947fc0e257136efa4f4feecc0662
Elvio Júnior <elvio.vieira95@gmail.com>
2022-11-13 12:22:38 -0300

    Reusing instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,10 +11,9 @@ public class Program {
 
     public static void main(String[] args) {
 
+        System.out.println("=== TEST 1 findById ===");
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
     }
 }
