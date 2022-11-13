8d39df0968f0eae202f14dab5b899eae468cd999
Elvio Júnior <elvio.vieira95@gmail.com>
2022-11-13 12:14:31 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,9 +11,10 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1,"Book");
-        Seller seller = new Seller(1,"Elvio","elvio@gmail.com",new Date(),3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
