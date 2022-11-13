4b3d3bb4d6b83068af3d9ccb43b25ceff2912db2
Elvio Júnior <elvio.vieira95@gmail.com>
2022-11-13 11:21:06 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Book");
+        Seller seller = new Seller(1,"Elvio","elvio@gmail.com",new Date(),3000.0, obj);
     }
 }
